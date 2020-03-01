1e3f8e3330599158162d17b83d71f03f1043fe4a
eunicLab <s1787807@FVFTXNHCJ1WKs-MacBook-Air.local>
2020-03-01 04:06:01 +0100

    Added unit tests

--- src/App.js
+++ src/App.js
@@ -1,105 +1,179 @@
 // Copyright (C) 2007-2019, GoodData(R) Corporation. All rights reserved.
 
 import React, { Component } from 'react';
+import './App.css';
 import '@gooddata/react-components/styles/css/main.css';
-
 import { ColumnChart } from '@gooddata/react-components';
+import { BarChart } from '@gooddata/react-components';
+import { AreaChart } from '@gooddata/react-components';
 
 const grossProfitMeasure = '/gdc/md/xms7ga4tf3g3nzucd8380o2bev8oeknp/obj/6877';
-const dateAttributeInMonths = '/gdc/md/xms7ga4tf3g3nzucd8380o2bev8oeknp/obj/2142';
+const dateAttributeInMonths =
+  '/gdc/md/xms7ga4tf3g3nzucd8380o2bev8oeknp/obj/2142';
 const dateAttribute = '/gdc/md/xms7ga4tf3g3nzucd8380o2bev8oeknp/obj/2180';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      defaultValue: '1',
+      defaultYear: '2015',
+      defaultChart: ColumnChart
+    };
+  }
 
-    getMonthFilter() {
-        return {
-            absoluteDateFilter: {
-                dataSet: {
-                    uri: dateAttribute
-                },
-                from: '2016-01-01',
-                to: '2016-01-31'
+  handleMonth = event => {
+    const value = event.target.value;
+    this.setState({ defaultValue: value });
+  };
+
+  handleYear = event => {
+    const value = event.target.value;
+    this.setState({ defaultYear: value });
+  };
+
+  handleChart = event => {
+    const value = event.target.value;
+    switch (value) {
+      case 'ColumnChart':
+        this.setState({ defaultChart: ColumnChart });
+        break;
+      case 'BarChart':
+        this.setState({ defaultChart: BarChart });
+        break;
+      case 'AreaChart':
+        this.setState({ defaultChart: AreaChart });
+        break;
+      default:
+        this.setState({ defaultChart: ColumnChart });
+    }
+  };
+
+  getMonthFilter() {
+    return {
+      absoluteDateFilter: {
+        dataSet: {
+          uri: dateAttribute
+        },
+        from: `${this.state.defaultYear}-${this.state.defaultValue}-01`,
+        to: `${this.state.defaultYear}-${this.state.defaultValue}-31`
+      }
+    };
+  }
+
+  getMeasures() {
+    return [
+      {
+        measure: {
+          localIdentifier: 'm1',
+          definition: {
+            measureDefinition: {
+              item: {
+                uri: grossProfitMeasure
+              }
             }
-
+          },
+          alias: '$ Gross Profit'
         }
-    }
+      }
+    ];
+  }
 
-    getMeasures() {
-        return [
-            {
-                measure: {
-                    localIdentifier: 'm1',
-                    definition: {
-                        measureDefinition: {
-                            item: {
-                                uri: grossProfitMeasure
-                            }
-                        }
-                    },
-                    alias: '$ Gross Profit'
-                }
-            }
-        ]
-    }
+  getViewBy() {
+    return {
+      visualizationAttribute: {
+        displayForm: {
+          uri: dateAttributeInMonths
+        },
+        localIdentifier: 'a1'
+      }
+    };
+  }
 
-    getViewBy() {
-        return {
-            visualizationAttribute:
-            {
-                displayForm: {
-                    uri: dateAttributeInMonths
-                },
-                localIdentifier: 'a1'
-            }
-        }
-    }
+  renderDropdown() {
+    return (
+      <select
+        defaultValue={this.state.defaultValue}
+        onChange={this.handleMonth}
+        className='selectMonth'>
+        <option value='1' className='selectMonth1'>
+          January
+        </option>
+        <option value='2'>February</option>
+        <option value='3'>March</option>
+        <option value='4'>April</option>
+        <option value='5'>May</option>
+        <option value='6'>June</option>
+        <option value='7'>July</option>
+        <option value='8'>August</option>
+        <option value='9'>September</option>
+        <option value='10'>October</option>
+        <option value='11'>November</option>
+        <option value='12'>December</option>
+      </select>
+    );
+  }
 
-    renderDropdown() {
-        return (
-            <select defaultValue="1">
-                <option value="1">January</option>
-                <option value="2">February</option>
-                <option value="3">March</option>
-                <option value="4">April</option>
-                <option value="5">May</option>
-                <option value="6">June</option>
-                <option value="7">July</option>
-                <option value="8">August</option>
-                <option value="9">September</option>
-                <option value="10">October</option>
-                <option value="11">November</option>
-                <option value="12">December</option>
-            </select>
-        )
-    }
+  renderYear() {
+    return (
+      <select
+        defaultValue={this.state.defaultYear}
+        onChange={this.handleYear}
+        className='selectYear'>
+        <option value='2015' className='selectYear1'>
+          2015
+        </option>
+        <option value='2016'>2016</option>
+        <option value='2017'>2017</option>
+      </select>
+    );
+  }
 
-    render() {
-        const projectId = 'xms7ga4tf3g3nzucd8380o2bev8oeknp';
-        const filters = [this.getMonthFilter()];
-        const measures = this.getMeasures();
-        const viewBy = this.getViewBy();
+  renderChartType() {
+    return (
+      <select
+        defaultValue={this.state.defaultChart}
+        onChange={this.handleChart}
+        className='selectChart'>
+        <option value='ColumnChart' className='selectChart1'>
+          Column Chart
+        </option>
+        <option value='BarChart'>Bar Chart</option>
+        <option value='AreaChart'>Area Chart</option>
+      </select>
+    );
+  }
 
-        return (
-            <div className="App">
-                <h1>$ Gross Profit in month {this.renderDropdown()} 2016</h1>
-                <div>
-                    <ColumnChart
-                        measures={measures}
-                        filters={filters}
-                        projectId={projectId}
-                    />
-                </div>
-                <h1>$ Gross Profit - All months</h1>
-                <div>
-                    <ColumnChart
-                        measures={measures}
-                        viewBy={viewBy}
-                        projectId={projectId}
-                    />
-                </div>
-            </div>
-        );
-    }
+  render() {
+    const projectId = 'xms7ga4tf3g3nzucd8380o2bev8oeknp';
+    const filters = [this.getMonthFilter()];
+    const measures = this.getMeasures();
+    const viewBy = this.getViewBy();
+
+    return (
+      <div className='App'>
+        <h1>
+          $ Gross Profit in month {this.renderDropdown()} {this.renderYear()}{' '}
+          {this.renderChartType()}
+        </h1>
+        <div className='chart'>
+          <this.state.defaultChart
+            measures={measures}
+            filters={filters}
+            projectId={projectId}
+          />
+        </div>
+        <h1>$ Gross Profit - All months</h1>
+        <div className='chart'>
+          <this.state.defaultChart
+            measures={measures}
+            viewBy={viewBy}
+            projectId={projectId}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,38 @@
 // Copyright (C) 2007-2019, GoodData(R) Corporation. All rights reserved.
 
 import React from 'react';
+import renderer from 'react-test-renderer';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('App component', () => {
+  it('starts with initial conditions January, 2015 and Column Chart', () => {
+    const wrapper = shallow(<App />);
+    const month = wrapper.find('option.selectMonth1').text();
+    const year = wrapper.find('option.selectYear1').text();
+    const chart = wrapper.find('option.selectChart1').text();
+    expect(month).toEqual('January');
+    expect(year).toEqual('2015');
+    expect(chart).toEqual('Column Chart');
+  });
+
+  it('when a change event occurs, select should update its value', () => {
+    const wrapper = shallow(<App />);
+    wrapper
+      .find('select.selectMonth')
+      .simulate('change', { target: { value: 'March' } });
+    wrapper
+      .find('select.selectYear')
+      .simulate('change', { target: { value: '2016' } });
+    wrapper
+      .find('select.selectChart')
+      .simulate('change', { target: { value: 'Bar Chart' } });
+  });
 });
